80ae8e0fd95af290ca2fe831be3f3998c51822f9
cartwrightforeman <cartwrightforeman@gmail.com>
2017-08-01 11:06:39 -0400

    create boardshowcontainer

--- react/src/App.js
+++ react/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Router, browserHistory, Route, IndexRoute } from 'react-router';
-import ListShowContainer from '../src/containers/ListShowContainer'
+import BoardShowContainer from '../src/containers/BoardShowContainer'
 
 const App = props => {
   return(
     <Router history={browserHistory}>
-      <Route path='/' component={ListShowContainer}/>
+      <Route path='/' component={BoardShowContainer}/>
     </Router>
   )
 };
---
+++ react/src/containers/BoardShowContainer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import ListShowContainer from '../containers/ListShowContainer'
+
+class BoardShowContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <ListShowContainer
+        />
+      </div>
+    )
+  }
+}
+
+export default BoardShowContainer;
