601f2f91d3f8f20894671fa67164e7ad02d4dbee
cartwrightforeman <cartwrightforeman@gmail.com>
2017-08-02 16:42:13 -0400

    dynamic styling

--- react/src/containers/BoardShowContainer.js
+++ react/src/containers/BoardShowContainer.js
@@ -23,13 +23,13 @@ class BoardShowContainer extends Component {
     return(
       <div>
         <div className="row collapse small-12">
-          <div className="happy list collapse small-3 column">
+          <div className="happy list collapse small-11 small-centered medium-8 medium-centered large-uncentered large-3 column">
             <ListShowContainer
               title = {"Happy"}
               listID = {1}
             />
           </div>
-          <div className="meh list small-3 column">
+          <div className="meh list small-11 small-centered medium-8 medium-centered large-uncentered large-3 column">
 
             <ListShowContainer
               title = {"Meh"}
@@ -37,14 +37,14 @@ class BoardShowContainer extends Component {
             />
           </div>
 
-          <div className="sad list small-3 column">
+          <div className="sad list small-11 small-centered medium-8 medium-centered large-uncentered large-3 column">
             <ListShowContainer
               title = {"Sad"}
               listID = {3}
             />
           </div>
 
-            <div className="double small-3 column">
+            <div className="double small-11 small-centered medium-8 medium-centered large-uncentered large-3 column">
             <div className="action list">
               <ListShowContainer
                 title = {"Action"}
