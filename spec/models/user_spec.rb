a362ee41a4160714fa83c66ab1257d4515957498
cartwrightforeman <cartwrightforeman@gmail.com>
2017-08-26 13:50:48 -0400

    fix tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe User, type: :model do
   it { should have_many :boards }
 
   it { should have_valid(:uid).when('213245') }
-  it { should have_valid(:name).when('Github Provides') }
   it { should have_valid(:email).when('Github@provides.com') }
   it { should have_valid(:image).when('githubprovidestheimage') }
 end
